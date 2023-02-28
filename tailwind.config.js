9531dbcb58f771ce7cc3069e28f0ad0bbe15c7a8
Lloyd Lobo <lloydlobo4@gmail.com>
2023-02-28 14:43:23 +0530

    feat: add a section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ['"Open Sans"', "sans-serif"],
-      cursive: ['"Gochi Hand"', "cursive"],
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        fontFamily: {
+            sans: ['"Open Sans"', "sans-serif"],
+            cursive: ['"Gochi Hand"', "cursive"],
+        },
+        extend: {
+            colors: {
+                primary: "#fbe094",
+                accent: "#f6ac9f",
+            },
+        },
     },
-    extend: {},
-  },
-  plugins: [],
+    variants: {},
+    plugins: [],
 };
