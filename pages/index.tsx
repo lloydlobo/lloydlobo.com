d86e364b2a136979b661e135dfa35c7f9032da53
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-01 21:58:50 +0530

    feat: style copy email to clipboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -566,17 +566,48 @@ export const EmailCopy = () => {
   const copyToClipboard = () => {
     navigator.clipboard.writeText(email);
     setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
   };
 
   return (
     <div>
       <div>
-        <div className="rounded-full text-xl text-primary ">{email}</div>
+        <div className="grid rounded-full text-xl text-primary ">{email}</div>
         <button
-          className=" w-full rounded-full text-end text-primary underline decoration-accent underline-offset-8 "
+          className="flex flex-row-reverse justify-between gap-0 text-end align-baseline text-primary"
           onClick={copyToClipboard}
         >
-          {isCopied ? "Copied!" : "Copy to clipboard"}
+          <>
+            {/* {isCopied ? "Copied!" : "Copy to clipboard"} */}
+
+            {isCopied ? (
+              <span className="text-accent">Copied</span>
+            ) : (
+              <span className="underline decoration-accent underline-offset-8 ">
+                Copy to clipboard
+              </span>
+            )}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={`${
+                isCopied ? "animate-pulse text-accent" : "brightness-75"
+              }
+              h-6 w-6 origin-center scale-[.66]`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+              />
+            </svg>
+          </>
         </button>
       </div>
     </div>
@@ -901,9 +932,6 @@ export default function IndexPage() {
                 </div>
                 <EmailCopy />
               </div>
-              {/* <button className="w-full rounded-full text-end text-xl text-primary">
-                hello@lloydlobo.com
-              </button> */}
             </div>
           </Section>
         </div>
