ed4c22c7c00fdc0687d93c7511639a9c23f1af8a
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-02 02:18:51 +0530

    feat: add navigation links in navbar

--- components/layout/NavbarDark.tsx
+++ components/layout/NavbarDark.tsx
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const navigation = [
-  // { name: "Dashboard", href: "#", current: true },
-  // { name: "Team", href: "#", current: false },
-  // { name: "Projects", href: "#", current: false },
-  // { name: "Calendar", href: "#", current: false },
+  { name: "Home", href: "/", current: true },
+  { name: "About", href: "/about", current: false },
+  { name: "Work", href: "/work", current: false },
+  { name: "Contact", href: "/contact", current: false },
 ];
 
 function classNames(...classes) {
@@ -15,14 +16,14 @@ function classNames(...classes) {
 
 export function NavbarDark() {
   return (
-    <Disclosure as="nav" className="bg-gray-800/10">
+    <Disclosure as="nav" className="bg-on-primary/90">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-secondary hover:bg-on-primary/50 hover:text-accent focus:outline-none focus:ring-2 focus:ring-inset focus:ring-accent">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -32,26 +33,35 @@ export function NavbarDark() {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+                <Link
+                  href="/"
+                  className="flex flex-shrink-0 items-center font-semibold decoration-accent underline-offset-8 hover:text-accent hover:underline"
+                >
                   Lloyd Lobo
-                </div>
+                </Link>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
-                    {navigation.map((item, index) => (
-                      <a
-                        key={item.name + index}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                    {navigation.map((item, index) => {
+                      if (item.name == "Home") {
+                        return null;
+                      } else {
+                        return (
+                          <a
+                            key={item.name + index}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? "bg-gray-900 text-primary"
+                                : "text-primary decoration-accent underline-offset-8 hover:text-accent hover:underline",
+                              "rounded-md px-3 py-2 text-sm font-medium"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        );
+                      }
+                    })}
                   </div>
                 </div>
               </div>
@@ -140,9 +150,13 @@ export function NavbarDark() {
                   as="a"
                   href={item.href}
                   className={classNames(
+                    // item.current
+                    //   ? "bg-gray-900 text-primary"
+                    //   : "text-primary decoration-accent underline-offset-8 hover:text-accent hover:underline",
+                    // "rounded-md px-3 py-2 text-sm font-medium"
                     item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      ? "bg-on-primary font-semibold text-accent underline  underline-offset-8"
+                      : "text-secondary hover:bg-on-primary hover:text-accent",
                     "block rounded-md px-3 py-2 text-base font-medium"
                   )}
                   aria-current={item.current ? "page" : undefined}
