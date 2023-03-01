1498d9e7173e294d5dc296bb01d15436f52e983c
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-02 02:38:04 +0530

    feat: add 404 page

--- pages/index.tsx
+++ pages/index.tsx
@@ -735,10 +735,56 @@ export default function IndexPage() {
               architect efficient code and tackle complex problems with ease.
             </p>
           </div>
+
+          {/* <div className="bg-on-primary py-24 sm:py-32"> */}
+          <div className="hidden bg-on-primary pt-12 sm:pt-16">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <h2 className="text-center text-lg font-semibold leading-8 text-primary">
+                Trusted by the world's most innovative teams
+              </h2>
+              <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 blur-[0.4px] brightness-[400%] invert sepia sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+                <img
+                  className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                  src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+                  alt="Transistor"
+                  width={158}
+                  height={48}
+                />
+                <img
+                  className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                  src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+                  alt="Reform"
+                  width={158}
+                  height={48}
+                />
+                <img
+                  className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                  src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+                  alt="Tuple"
+                  width={158}
+                  height={48}
+                />
+                <img
+                  className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                  src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+                  alt="SavvyCal"
+                  width={158}
+                  height={48}
+                />
+                <img
+                  className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                  src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+                  alt="Statamic"
+                  width={158}
+                  height={48}
+                />
+              </div>
+            </div>
+          </div>
         </Section>
 
         <Section>
-          <div className="mx-auto w-full text-secondary sm:pt-32">
+          <div className="mx-auto w-full text-secondary">
             <div className="relative mx-auto w-full">
               <p className="mb-20 w-[25vw] border-b-[6px] border-secondary py-4 text-start text-xl font-normal">
                 Lloyd Lobo
