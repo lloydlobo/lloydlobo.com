93558a8720d2b0e2e7c4aae87403e87d82149580
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-16 10:48:50 +0530

    feat: use animate onmousemove blob fx

--- components/effects/MouseBlob.tsx
+++ components/effects/MouseBlob.tsx
@@ -23,8 +23,16 @@ export function MouseBlob() {
     document.body.onpointermove = (event) => {
       const { clientX, clientY } = event;
 
-      blob.style.left = `${clientX}px`;
-      blob.style.top = `${clientY}px`;
+      // blob.style.left = `${clientX}px`;
+      // blob.style.top = `${clientY}px`;
+
+      // For delayed and relaxed effect use animate API.
+      // `fill: "forwards"` - Do not reset the property after the duration end.
+      //
+      blob.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`,
+      }, { duration: 300, fill: "forwards" });
     };
 
     return () => { };
@@ -41,10 +49,12 @@ export function MouseBlob() {
           }
         `}
       </style>
-      <div
-        id="blob"
-        className="absolute aspect-square h-[500px] bg-gradient-to-t from-gray1 to-green-300 dark:from-on-primary dark:to-green-400 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[50%] blur-3xl dark:brightness-[25%]  dark:mix-blend-screen disabled:mix-blend-multiply opacity-95 -z-[9999999]"
-      ></div>
+      <div className="-z-50 brightness-75 blur-3xl w-screen min-h-screen absolute">
+        <div
+          id="blob"
+          className="absolute aspect-square h-[500px] bg-gradient-to-t from-gray1 to-green-300 dark:from-on-primary dark:to-green-400 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[50%] disabled:blur-3xl disabled:dark:brightness-[25%]  disabled:dark:mix-blend-screen disabled:mix-blend-multiply opacity-95 -z-[9999999]"
+        ></div>
+      </div>
     </>
   );
 }
